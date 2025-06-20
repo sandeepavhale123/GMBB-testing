01bf10d962288f67748c4162f66db7945172839a
Ayushi9521 <79744303+Ayushi9521@users.noreply.github.com>
2025-06-20 15:32:10 +0530

    onboarding flag rest done

--- src/api/listingApi.ts
+++ src/api/listingApi.ts
@@ -16,6 +16,9 @@ export const enableDisableListings = async (
   payload: EnableDisableListingsPayload
 ): Promise<EnableDisableListingsResponse> => {
   const response = await axiosInstance.post("/change-listing-status", payload);
-  console.log("listing status response", response);
+  console.log("listing status response", response.data.status);
+  if (response.data.status === 200) {
+    localStorage.setItem("onboarding", 0);
+  }
   return response.data;
 };
