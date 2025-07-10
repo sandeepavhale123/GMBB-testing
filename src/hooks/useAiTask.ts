8c9d6b40be616e918a56cdd3dc4a6682fc6ef6b5
Ayushi9521 <79744303+Ayushi9521@users.noreply.github.com>
2025-07-10 12:01:57 +0530

    spelling fixed

--- src/hooks/useAiTask.ts
+++ src/hooks/useAiTask.ts
@@ -1,5 +1,5 @@
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import { aitaskApi, AiTask, TaskStats } from "../api/aiTaskApi";
+import { aitaskApi, AiTask, TaskStats } from "../api/aitaskApi";
 
 interface UseAiTasksOptions {
   listingId?: number;
