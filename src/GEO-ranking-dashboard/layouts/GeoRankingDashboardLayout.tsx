bf1a855c989e1d88958ec0c99bd18320589f0dd3
Vijaysalve <vijays.webmart@gmail.com>
2025-08-25 11:35:57 +0530

    added image on coming soon page

--- src/GEO-ranking-dashboard/layouts/GeoRankingDashboardLayout.tsx
+++ src/GEO-ranking-dashboard/layouts/GeoRankingDashboardLayout.tsx
@@ -4,6 +4,7 @@ import { Outlet } from 'react-router-dom';
 export const GeoRankingDashboardLayout: React.FC = () => {
   return (
     <div className="min-h-screen bg-background">
+      
       <Outlet />
     </div>
   );
