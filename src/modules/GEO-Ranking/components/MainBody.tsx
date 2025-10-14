03c9b6b4a1a3541d5c0edf63667be069c65feb69
Vijaysalve <vijays.webmart@gmail.com>
2025-10-14 14:40:58 +0530

    adding padding bottom in MainBody component on multi location dashboard layout

--- src/modules/GEO-Ranking/components/MainBody.tsx
+++ src/modules/GEO-Ranking/components/MainBody.tsx
@@ -5,7 +5,7 @@ interface MainBodyProps {
 export const MainBody: React.FC<MainBodyProps> = ({
   children
 }) => {
-  return <main className="flex-1 min-h-[90vh] w-full bg-background pt-32 md:pt-32  lg:pt-32">
+  return <main className="flex-1 min-h-[90vh] w-full bg-background pt-32 md:pt-32  lg:pt-32 pb-[100px] md:pb-[100px]  lg:pb-[100px]">
       <div className="max-w-7xl mx-auto px-4 py-6">{children}</div>
     </main>;
 };
--- src/modules/live-seo-fixer/components/MainBody.tsx
+++ src/modules/live-seo-fixer/components/MainBody.tsx
@@ -8,7 +8,7 @@ export const MainBody: React.FC<MainBodyProps> = ({
   children
 }) => {
   return (
-    <main className="flex-1 min-h-[90vh] w-full bg-background pt-32 md:pt-32 lg:pt-32">
+    <main className="flex-1 min-h-[90vh] w-full bg-background pt-32 md:pt-32 lg:pt-32 pb-[100px] md:pb-[100px]  lg:pb-[100px]">
       <div className="max-w-7xl mx-auto px-4 py-6">{children}</div>
     </main>
   );
