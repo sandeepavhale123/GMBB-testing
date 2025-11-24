a755bea30c193740b5cb63337694d2e552dd3cc8
Vijaysalve <vijays.webmart@gmail.com>
2025-11-24 12:09:09 +0530

    Fixed the tabs broken issue on all the page

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -13,7 +13,7 @@ const TabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "inline-flex h-10 items-center justify-center rounded-md bg-muted p-1 text-muted-foreground overflow-x-auto scrollbar-hide",
+      "inline-flex h-10 items-center justify-center rounded-md bg-muted p-1 text-muted-foreground p-1",
       className
     )}
     {...props}
@@ -28,7 +28,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center justify-center whitespace-nowrap rounded-none px-2 py-2 text-xs sm:px-4 sm:py-3 sm:text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:text-primary data-[state=active]:border-b-2 data-[state=active]:border-primary border-b-2 border-transparent",
+      "inline-flex h-8 rounded rounded-sm items-center justify-center whitespace-nowrap  px-2 py-1 text-xs sm:px-4 sm:py-3 sm:text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:text-primary data-[state=active]:bg-white",
       className
     )}
     {...props}
