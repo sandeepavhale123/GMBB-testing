8885b831ea3bb18a347141d8ecf693728246a131
Vijaysalve <vijays.webmart@gmail.com>
2025-12-19 14:35:06 +0530

    update the total review card

--- src/multiDashboardLayout/components/settings/ActivityWrapper.tsx
+++ src/multiDashboardLayout/components/settings/ActivityWrapper.tsx
@@ -17,11 +17,15 @@ export const ActivityWrapper: React.FC = () => {
             {t("description")}
           </p>
         </div>
-        <div className="flex items-center gap-2">
+        {/* <div className="flex items-center gap-2">
           <span className="text-sm text-muted-foreground">{t("totalReplies")}:</span>
           <Badge variant="secondary" className="text-lg px-3 py-1">
             {isLoading ? "..." : pagination.total}
           </Badge>
+        </div> */}
+         <div className="flex items-center gap-2 bg-blue-100 p-3 rounded-md mb-0">
+            <p className="text-sm text-muted-foreground">{t("totalReplies")}:</p>
+            <p className="bg-blue-500 text-white  rounded px-2 py-1 text-sm  ">{isLoading ? " " : pagination.total}</p>
         </div>
       </div>
       <TeamActivityLogs showMemberFilter={true} />
